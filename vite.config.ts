a0d90ce7bece719641360e317ed9f10874873dc5
yooyjsj-spec <yooyjsj@gmail.com>
2025-12-18 00:46:29 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        base: '/ADAML/',
         port: 3000,
         host: '0.0.0.0',
       },
